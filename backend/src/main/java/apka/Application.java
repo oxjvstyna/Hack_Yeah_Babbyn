d5bb5b11919ce363e806d1414f08e10dc6e698b9
Justyna Orłowska <jorlowska2003@gmail.com>
2025-10-04 15:31:07 +0200

    v1

--- backend/src/main/java/apka/Application.java
+++ backend/src/main/java/apka/Application.java
@@ -1,5 +1,11 @@
 package apka;
 
-@SpringBootApplication()
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "apka")
 public class Application {
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
 }
