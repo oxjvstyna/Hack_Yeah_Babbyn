c2c676aa08848b08990c273205049b783d9d6d8a
Małgorzata Krupanek <66110734+GosiaKk5@users.noreply.github.com>
2025-10-04 16:19:26 +0200

    Backend init (#1)
    * added spring boot
    
    * added spring boot
    
    * commit1
    
    * dependencies
    
    * added spring boot
    
    * v1
    
    ---------
    
    Co-authored-by: Justyna Orłowska <jorlowska2003@gmail.com>

---
+++ backend/src/main/java/apka/controller/BabbynController.java
@@ -0,0 +1,24 @@
+package apka.controller;
+
+import apka.service.BabbynService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BabbynController {
+
+    private final BabbynService service;
+
+    public BabbynController(BabbynService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        service.test();
+        return "ok";
+    }
+}
+
---
+++ backend/src/main/java/apka/service/BabbynService.java
@@ -0,0 +1,14 @@
+package apka.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BabbynService {
+
+    public BabbynService() {}
+
+    public void test() {
+        System.out.println("test");
+    }
+
+}
