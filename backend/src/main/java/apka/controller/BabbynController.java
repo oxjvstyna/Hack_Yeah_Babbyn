710c5ef1d5d8a4b1a1617a03fa2f9abb4f562531
Weronika <weronikaw@remitly.com>
2025-10-04 16:40:34 +0200

    fix controller

--- backend/src/main/java/apka/controller/BabbynController.java
+++ backend/src/main/java/apka/controller/BabbynController.java
@@ -4,6 +4,7 @@ import apka.model.User;
 import apka.service.BabbynService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class BabbynController {
     }
 
     @PostMapping("place")
-    public String test(User user) {
+    public String test(@RequestBody User user) {
         service.test();
         System.out.println(user.toString());
         return "ok";
