7dd9fce38f5dc85006cc6d9f7207b66b6834a97f
Olciaaa <ola.poskrobek@gmail.com>
2025-10-05 06:38:15 +0200

    gallery change

--- Trippin/components/ModalFriendsTab.tsx
+++ Trippin/components/ModalFriendsTab.tsx
@@ -1,39 +1,37 @@
 import React from "react";
-import Star from "./Star";
 import { View, Text, Image, ScrollView } from "react-native";
 
 export default function ModalFriendsTab({ styles, ...props }) {
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <Text style={styles.sectionTitle}>Friends who visited</Text>
-      <CountryFriends friends={[]} styles={styles}/>
+      <CountryFriends friends={[]} styles={styles} />
       <Text style={styles.sectionTitle}>Friends gallery</Text>
       <View style={styles.galleryGrid}>
-              {[1, 2, 3].map((id) => (
-                <Image
-                  key={id}
-                  source={require("@/assets/images/aa.png")}
-                  style={styles.galleryImage}
-                  resizeMode="cover"
-                />
-              ))}
-            </View>
+        {[1, 2, 3].map((id) => (
+          <Image
+            key={id}
+            source={require("@/assets/images/aa.png")}
+            style={styles.galleryImage}
+            resizeMode="cover"
+          />
+        ))}
+      </View>
     </ScrollView>
   );
 }
 
-
-function CountryFriends({friends, styles, ...props}) {
+function CountryFriends({ friends, styles, ...props }) {
   return (
     <View style={styles.friendContainer}>
       {[1, 2, 3].map((id) => (
-          <Image
-            key={id}
-            source={require("@/assets/images/aa.png")}
-            style={styles.friendImage}
-            resizeMode="cover"
-          />
-        ))}
+        <Image
+          key={id}
+          source={require("@/assets/images/aa.png")}
+          style={styles.friendImage}
+          resizeMode="cover"
+        />
+      ))}
     </View>
-  )
+  );
 }
--- Trippin/components/Star.tsx
+++ Trippin/components/Star.tsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Rating } from "react-native-ratings";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { funRating, secRating } from "@/hooks/api";
 
 type StarProps = {
   title: string;
+  iso: string;
+  ratingStart: number;
+  isFun: boolean;
 };
 
-export default function Star({ title }: StarProps) {
-  const [rating, setRating] = useState<number>(0);
+export default function Star({ title, iso, ratingStart, isFun }: StarProps) {
+  const [rating, setRating] = useState<number>(ratingStart);
+
+  useEffect(() => {
+    if (rating !== ratingStart) {
+      if (isFun) funRating(iso, rating);
+      else secRating(iso, rating);
+    }
+  }, [rating]);
 
   return (
-    <>
+    <View>
       <Text style={{ fontSize: 18, color: "black" }}>{title}</Text>
       <Rating
         style={{ alignItems: "flex-start" }}
@@ -18,11 +29,14 @@ export default function Star({ title }: StarProps) {
         ratingCount={5}
         startingValue={rating}
         imageSize={30}
-        onFinishRating={(v: React.SetStateAction<number>) => setRating(v)}
+        onSwipeRating={(v) => setRating(v)}
+        // onFinishRating={(v: number) => {
+        //   setRating(v);
+        // }}
         tintColor="#fff"
         ratingBackgroundColor="#e5e7eb"
-        // readonly={true}
+        // readonly={rating > 0}
       />
-    </>
+    </View>
   );
 }
